3563dbc0d5e61bb7ef8d1ad8979ea68798d87ec9
DaniyalBabar22 <147255848+DaniyalBabar22@users.noreply.github.com>
2023-12-11 13:52:08 +0500

    dashboards completed
    dashboards completed

---
+++ src/app/modules/admin/dashboards/crm-dashboard/crm-dashboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CrmDashboardComponent } from './crm-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+  
+    CrmDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CrmDashboardModule { }
