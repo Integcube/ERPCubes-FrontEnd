b3f7cf93663eb256e4d5045bd7383f1334e7e8a7
Jalalnawas40 <154512370+Jalalnawas40@users.noreply.github.com>
2024-04-03 12:10:00 +0500

    all
    all

--- src/app/modules/admin/checklist/checklist-report/checklist-report.component.ts
+++ src/app/modules/admin/checklist/checklist-report/checklist-report.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { UntypedFormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Subject } from 'rxjs';
+import { ChecklistReporttService } from './checklist-report.service';
 
 @Component({
   selector: 'checklist-report',
@@ -7,7 +12,23 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChecklistReportComponent {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('exporter') public exporter;
 
+  dataSource: MatTableDataSource<LeadSourceReport>;
+  displayedColumns: string[] = [ 'source', 'totalLeads', 'convertedLeads', 'conversionRate', 'averageDealSize', 'totalRevenue'];
+  leadSourceReportCount: number = 0;
+  totalLeadSource: number = 0;
+  searchInputControl: UntypedFormControl = new UntypedFormControl();
+  
+
+  _unsubscribeAll: Subject<any> = new Subject<any>();
+  constructor(
+    private _leadSourceReportService: ChecklistReporttService,
+    private _changeDetectorRef: ChangeDetectorRef,
+    
+  ) { }
   
   }
   
